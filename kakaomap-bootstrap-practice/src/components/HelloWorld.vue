ce78dcac9c912e68416f714bfb74c4b4bb7d0638
parkjunsoo <01junsoo@gmail.com>
2021-03-04 12:38:33 +0900

    bootstrap+kakao practice

---
+++ kakaomap-bootstrap-practice/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+    <b-button>Button</b-button>
+    <b-button variant="danger">Button</b-button>
+    <b-button variant="success">Button</b-button>
+    <b-button variant="outline-primary">Button</b-button>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
